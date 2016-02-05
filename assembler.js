9d7b5f2d74c03d72d8ee951fa3554819c1df7ad0
LearningNerd <learningnerd@gmail.com>
2016-02-04 20:05:34 -0800

    finished version 1 of assembler, but it isn't handling comments properly yet!

--- assembler.js
+++ assembler.js
@@ -1,51 +1,178 @@
 (function () {
 	// VERSION 1: for .asm files with NO symbols or labels, only constants
-	function assemble (asmProgram) {
+	function assemble (asmProgram) {		
+		var assemblerOutput = '';
 		
-		var assemblerOutputArray = [];
+		// run through the assembly code one line at a time
 		var instructionArray = asmProgram.split('\n');
-		
 		instructionArray.forEach( function (instruction) {
-			// do stuff to parse and translate here!
-		});
-		
-		var parser = {};
+			instruction = removeWhitespace(instruction);
+			console.log(instruction);
+			instruction = removeComments(instruction);			
+			console.log(instruction);
+			if (commandType(instruction) === 'A') {
+				console.log('A INSTRUCTION!');
+				console.log(instruction);
+				// convert A-instructions to binary representation
+				assemblerOutput += getBinary16( getSymbol(instruction) ) + '\n';				
+			} else if (commandType(instruction) === 'C') {
+				console.log('C INSTRUCTION!');
+				console.log(instruction);
+				// convert C-instructions to binary representation
+				assemblerOutput += getCInstructMachineCode( operationFields(instruction) ) + '\n';
+				console.log("assembler output: ");
+				console.log(assemblerOutput);
+			}
+		});			
 
-		parser.commandType = function(str) {
-			// takes an instruction string, returns A, C, or L for command types
-		};
+		function removeWhitespace (str) {
+			return str.replace(/\s+/g, '');
+		}
 		
-		parser.symbol = function(str) {
-			// takes an instruction string, returns symbol/decimal number of A or L
-		};
+		function removeComments(str) {
+			return str.replace(/\/\/.*$/g, '');
+		}
 		
-		parser.dest = function(str) {
-			// takes an instruction string, returns 1 of 8 dest mnemonic strings from C
-		};
+		// takes a command, returns A or C for instruction types. TODO: return L for labels
+		function commandType(str) {
+			if (str.charAt(0) === '@') {
+				return 'A';
+			} else {
+				return 'C';
+			}
+		}
 		
-		parser.comp = function(str) {
-			// takes an instruction string, returns 1 of 28 comp mnemonic strings from C
-		};
+		// takes an instruction string, returns decimal number of A-instruction. TODO: handle symbols and labels
+		function getSymbol(str) {
+			return str.slice(1);
+		}
 		
-		parser.jump = function(str) {
-			// takes an instruction string, returns 1 of 8 jump mnemonic strings from C
-		};
+		// takes an instruction string, returns object identifying its fields
+		function operationFields(str) {
+			var fields = {};
+			var equalsSplit = str.split(/=/);
+			if (equalsSplit.length == 1) {
+				// no equal sign, so this is a comp;jump code
+				var semicolonSplit = equalsSplit[0].split(/;/);
+				fields.comp = semicolonSplit[0];
+				fields.jump = semicolonSplit[1];
+				fields.dest = null;
+			} else if (equalsSplit.length == 2) {
+				var semicolonSplit = equalsSplit[1].split(/;/);
+				if (semicolonSplit.length == 1) {
+					// no semicolon, so this is a dest=comp code
+					fields.dest = equalsSplit[0];
+					fields.comp = semicolonSplit[0];
+					fields.jump = null;
+				} else if (semicolonSplit.length == 2) {
+					// has equals and semicolon, so this is a dest=comp;jump code
+					fields.dest = equalsSplit[0];
+					fields.comp = semicolonSplit[0];
+					fields.jump = semicolonSplit[1];
+				}
+				
+			}
+			console.log("fields: ");
+			console.log(fields);
+			return fields;
+		}
+						
+		// takes a mnemonic field from C instruction, returns 3-bit binary code for dest
+		function dest(field) {
+			var dest = {
+				'null': '000',
+				'M':    '001',
+				'D':    '010',
+				'MD':   '011',
+				'A':    '100',
+				'AM':   '101',
+				'AD':   '110',
+				'AMD':  '111'
+			};
+			return dest[field];
+		}
+
+		// takes a mnemonic field from C instruction, returns 7-bit binary code for comp
+		function comp (field) {
+			var comp = {
+				'0':    '0101010',
+				'1':    '0111111',
+				'-1':   '0111010',
+				'D':    '0001100',
+				'A':    '0110000',
+				'!D':   '0001101',
+				'!A':   '0110001',
+				'-D':   '0001111',
+				'-A':   '0110011',
+				'D+1':  '0011111',
+				'A+1':  '0110111',
+				'D-1':  '0001110',
+				'A-1':  '0110010',
+				'D+A':  '0000010',
+				'D-A':  '0010011',
+				'A-D':  '0000111',
+				'D&A':  '0000000',
+				'D|A':  '0010101',
+				'M':    '1110000',
+				'!M':   '1110001',
+				'-M':   '1110011',
+				'M+1':  '1110111',
+				'M-1':  '1110010',
+				'D+M':  '1000010',
+				'D-M':  '1010011',
+				'M-D':  '1000111',
+				'D&M':  '1000000',
+				'D|M':  '1010101'        
+			};
+			return comp[field];
+		}
+
+		// takes a mnemonic field from C instruction, returns 3-bit binary code for jump
+		function jump (field) {
+			var jump = {
+				'null': '000',
+				'JGT':  '001',
+				'JEQ':  '010',
+				'JGE':  '011',
+				'JLT':  '100',
+				'JNE':  '101',
+				'JLE':  '110',
+				'JMP':  '111'
+			};
+			return jump[field];
+		}
 		
-		var translator = {};
+		// takes an object containing C-instruction fields, returns full machine code instruction
+		function getCInstructMachineCode (fields) {
+			var destCode = dest(fields.dest);
+			var compCode = comp(fields.comp);
+			var jumpCode = jump(fields.jump);
+			console.log("comp: ");
+			console.log(compCode);
+			console.log("dest: ");
+			console.log(destCode);
+			console.log("jump: ");
+			console.log(jumpCode);
+			// append complete C-instruction machine code as a line in the assembler's output string
+			return '111' + compCode + destCode + jumpCode;
+		}
 		
-		translator.dest = function(field) {
-			// takes a mnemonic field from C instruction, returns 3-bit binary code for dest
-		};
+		// takes a string representing a decimal number, returns a string representing a 16-bit binary number
+		function getBinary16 (decimalString) {
+			var decimalNum = parseInt(decimalString, 10);
+			var binaryString = decimalNum.toString(2);
+			if (binaryString.length > 16) {
+				// if larger than 16 bits in binary, truncate the string
+				return binaryString.slice(0,16);
+			}
+			// pad with leading zeros if needed
+			while (binaryString.length < 16) {
+				binaryString = '0' + binaryString;
+			}
+			return binaryString;
+		}
 		
-		translator.comp = function(field) {
-			// takes a mnemonic field from C instruction, returns 7-bit binary code for comp
-		};
-		
-		translator.jump = function(field) {
-			// takes a mnemonic field from C instruction, returns 3-bit binary code for jump
-		};
-		
-		return asmProgram; // need to change this to output newly generated machine code instead!
+		return assemblerOutput.trim();
 		
 	}; // end of assembler
 
@@ -83,7 +210,7 @@
 		create.style.display = 'none';
 	}, false);
 	
-	// When links is downloaded, reset buttons:
+	// When link is downloaded, reset buttons:
 	downloadlink.addEventListener('click', function () {
 		// Display create link, hide download link
 		downloadlink.style.display = 'none';
